add669a678403bc3bc2a6566757f140b8eb713d3
Ennis <A0095091@live.tees.ac.uk>
2023-02-28 16:12:43 +0000

    HealthPickup

--- Source/JourneymanT6/Private/Pickups/HealthPotion.cpp
+++ Source/JourneymanT6/Private/Pickups/HealthPotion.cpp
@@ -2,10 +2,32 @@
 
 
 #include "Pickups/HealthPotion.h"
+#include "TestingCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+
+
 
 void AHealthPotion::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	ATestingCharacter* Player = Cast<ATestingCharacter>(OtherActor);
 
 
+	if (Player->CurrentHealth <= 2.0f)
+	{
+		Heal();
+		Player->HealPlayer();
+	}
+	
+	
+
+}
+
+void AHealthPotion::Heal()
+{
+	
+	UGameplayStatics::PlaySound2D(GetWorld(), PickupSound);
+	
+	Destroy();
 }
